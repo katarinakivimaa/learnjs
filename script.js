af5158467afe7da2d4a05d90ff5c587db4a91ac7
Katarina Kivimaa <katarina.kivimaa@gmail.com>
2023-09-18 09:41:29 +0300

    Add examples of loops

--- script.js
+++ script.js
@@ -1,74 +1,70 @@
-let num = 2;
+let num = 10;
 
+if(num > 10) {
+    console.log('Suurem');
+} else if (num===10) {
+    console.log('Võrdne')
+}   else {
+    console.log('Väiksem');
+}
 
+let day = 4;
 
-num += 3;
-num -= 4;
-num*= 3;
-num/= 8;
-num++;
-num--;
-num = 12 % 2;
-num = 2;
-num = num**3;
-console.log(typeof num);
-let string = `Hello World
-vevwsefaqwsbf`;
-string += '!!!';
-console.log(string);
-console.log(typeof string);
-let bool = true;
-bool = false;
+switch(day){
+    case 0:
+        console.log('Esmaspäev');
+        break;
+    case 1:
+        console.log('Teisipäev');
+        break;
+    case 2:
+        console.log('Kolmapäev');
+        break;
+    case 3:
+        console.log('Neljapäev');
+        break;
+    case 4:
+        console.log('Reede');
+        break;
+    case 5:
+    case 6:
+        console.log('Nädalavahetus');
+        break;
+    default:
+        console.log('Imelik päev');
+        break;
+}
 
-bool = true && false;
-bool = false || true;
-bool = !bool;
-bool = false && true || true && !false;
-num = 10;
-bool = 2 > num;
-bool = 10 != num;
-bool = '10' == num;
-bool = '10' === num;
-bool = '10' !== num;
+for(let i=1_000_000;i>1;i/=2){
+    console.log(i);
+}
 
-console.log(bool);
-console.log(typeof bool);
+// let minutes = new Date().getMinutes();
+// console.log(minutes);
+// while(minutes+1 != new Date().getMinutes()) {
+//     console.log('old minute');
 
-let noValue = null;
-let noValue2;
-console.log(noValue2); // undefined
-num = -1/0;
-num = 1/0;
-num = 0 * -1;
-num = 0.1 + 0.2;
-console.log(num);
-let array = [1, 2, 3, 4];
-array = [1, 'hello', 3, 4];
-console.log(array);
-console.log(array[0]);
-array[0] = 7;
-console.log(array);
-array = [
-    [1,2,3],
-    [4,5,6]
-];
-console.log(array);
-console.log(array[1][2]);
-array = [[[[[5]]]]];
-let obj = {
-    name: 'kaspar',
-    age: 30,
-    eyeColor: 'blue',
-    favpets: ['dogs', 'cats', 'rats']
-};
-console.log(obj);
-console.log(obj.name);
-console.log(obj.favpets[1]);
-console.log(console);
-console.log(array.length);
-console.log(string.length);
-console.log(string[0]);
+// }
+// console.log('new minute has started');
+let i = 1;
+while(i>10) {
+    console.log('while ' + i);
+}
 
+i = 1;
+do {
+    console.log('do ' + i);
+} while (i>10);
+
+let fruits = ['apple', 'cherry', 'strawberry', 'pear', 'melon'];
+
+for(let i = 0; i<fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+fruits.forEach(function (fruit){
+    console.log(fruit);
+});
 
 
 
